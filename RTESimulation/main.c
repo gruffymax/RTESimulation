41414db00a53841035cb9fcb319f31d928d7f084
gwaym <gwaym@DESKTOP-S6423KN>
2021-03-04 15:45:32 +0000

    Console Demo

--- RTESimulation/main.c
+++ RTESimulation/main.c
@@ -1,7 +1,35 @@
 #include <stdio.h>
 #include <Windows.h>
+#include <string.h>
+#include "belt.h"
 
 int main(void)
 {
-	return 0;
+    HANDLE hStdout, hNewScreenBuffer;
+    hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    hNewScreenBuffer = CreateConsoleScreenBuffer(
+        GENERIC_READ |           // read/write access
+        GENERIC_WRITE,
+        FILE_SHARE_READ |
+        FILE_SHARE_WRITE,        // shared
+        NULL,                    // default security attributes
+        CONSOLE_TEXTMODE_BUFFER, // must be TEXTMODE
+        NULL);                   // reserved; must be NULL
+
+    COORD pos = { 5, 2 };
+
+    char buffer[50];
+    strcpy_s(buffer, 50, "Hello World");
+    SetConsoleCursorPosition(hNewScreenBuffer, pos);
+    WriteConsoleA(hNewScreenBuffer, buffer, strlen(buffer), NULL, NULL);
+
+    getch();
+
+    SetConsoleActiveScreenBuffer(hNewScreenBuffer);
+
+    getch();
+    SetConsoleActiveScreenBuffer(hStdout);
+
+    return 0;
 }
