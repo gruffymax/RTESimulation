d7ce838787b2450d78f7cc80ad87a4afc9366a71
Aaron Whiteaway <aaronwhiteaway@btinternet.com>
2021-03-08 13:33:47 +0000

    Added basic menu and conveyor prototype functions

--- RTESimulation/main.c
+++ RTESimulation/main.c
@@ -1,35 +1,112 @@
 #include <stdio.h>
 #include <Windows.h>
 #include <string.h>
-#include "belt.h"
+#include <stdlib.h>
+#include <time.h>
+#include <windows.h>
+#include <conio.h>
+//#include "belt.h"
 
-int main(void)
+
+//prototypes
+int ConveyorOne();
+int ConveyorTwo();
+
+//Global variables
+int LargeBlock;
+int SmallBlock;
+
+
+//main
+int main()
 {
-    HANDLE hStdout, hNewScreenBuffer;
-    hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
+    int a = 1;
+    int b = 2;
+    int c = 3;
+    int choice;
+    
 
-    hNewScreenBuffer = CreateConsoleScreenBuffer(
-        GENERIC_READ |           // read/write access
-        GENERIC_WRITE,
-        FILE_SHARE_READ |
-        FILE_SHARE_WRITE,        // shared
-        NULL,                    // default security attributes
-        CONSOLE_TEXTMODE_BUFFER, // must be TEXTMODE
-        NULL);                   // reserved; must be NULL
 
-    COORD pos = { 5, 2 };
 
-    char buffer[50];
-    strcpy_s(buffer, 50, "Hello World");
-    SetConsoleCursorPosition(hNewScreenBuffer, pos);
-    WriteConsoleA(hNewScreenBuffer, buffer, strlen(buffer), NULL, NULL);
+    MENU:
+    printf("Please select the option you wish to choose:\n");
+    printf("[1] - Conveyor 1\n");
+    printf("[2] - Conveyor 2\n");
+    printf("[3] - Shutdown\n");
+    scanf_s("%d", &choice);
+    
 
-    getch();
-
-    SetConsoleActiveScreenBuffer(hNewScreenBuffer);
-
-    getch();
-    SetConsoleActiveScreenBuffer(hStdout);
-
-    return 0;
+    if (choice == 1)
+    {
+       // system("cls");
+        //printf("1");
+        ConveyorOne();
+    }
+    else if (choice == 2)
+    {
+        system("cls");
+        printf("2");
+    }
+    else if (choice == 3)
+    {
+        system("cls");
+        printf("3");
+    }
+    else
+    {
+        printf("Wrong input\n");
+        system("cls");
+        goto MENU;
+    }
 }
+
+int ConveyorOne()
+{
+    printf("Conveyor 1:\n");
+    printf("No. Large blocks:  %d", LargeBlock);
+    printf("No. Small blocks:  %d", LargeBlock);
+
+
+}
+
+int ConveyorTwo()
+{
+
+}
+
+
+
+
+
+
+
+
+
+
+/*HANDLE hStdout, hNewScreenBuffer;
+hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
+
+hNewScreenBuffer = CreateConsoleScreenBuffer(
+    GENERIC_READ |           // read/write access
+    GENERIC_WRITE,
+    FILE_SHARE_READ |
+    FILE_SHARE_WRITE,        // shared
+    NULL,                    // default security attributes
+    CONSOLE_TEXTMODE_BUFFER, // must be TEXTMODE
+    NULL);                   // reserved; must be NULL
+
+COORD pos = { 5, 2 };
+
+char buffer[50];
+strcpy_s(buffer, 50, "Hello World");
+SetConsoleCursorPosition(hNewScreenBuffer, pos);
+WriteConsoleA(hNewScreenBuffer, buffer, strlen(buffer), NULL, NULL);
+
+getch();
+
+SetConsoleActiveScreenBuffer(hNewScreenBuffer);
+
+getch();
+SetConsoleActiveScreenBuffer(hStdout);
+
+return 0;*/
