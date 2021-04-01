756ab386a915e17bf35cf66a4ec57d925277ad41
Gareth Waymark <gwaymark@gmail.com>
2021-04-01 15:23:01 +0100

    Added mutex functions

--- RTESimulation/semphr.c
+++ RTESimulation/semphr.c
@@ -1,1 +1,28 @@
 #include "semphr.h"
+
+char* create_mutex(void)
+{
+	char* mutex = malloc(sizeof(char));
+	*mutex = 1; //Mutex available
+	return mutex;
+}
+
+char take_mutex(char* mutex_handle)
+{
+	if (*mutex_handle == 0)
+	{
+		*mutex_handle = 0;
+		return 0;
+	}
+	return 1; //Error
+}
+
+char give_mutex(char* mutex_handle)
+{
+	if (*mutex_handle == 0)
+	{
+		*mutex_handle = 1;
+		return 0;
+	}
+	return 1; //Error
+}
--- RTESimulation/semphr.h
+++ RTESimulation/semphr.h
@@ -1,6 +1,10 @@
 #ifndef SEMPHR_H
 #define SEMPHR_H
 
+#include <stdint.h>
 
+char* create_mutex(void);
+char take_mutex(char* mutex_handle);
+char give_mutex(char* mutex_handle);
 
 #endif // SEMPHR_H
